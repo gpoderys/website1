1f09e8197ac613d175562e1199ec1afc8c1cb578
Gintautas Poderys <gintautasp25@gmail.com>
2023-08-18 22:28:03 +0300

    added JS

--- Script1.js
+++ Script1.js
@@ -1,1 +1,14 @@
 // JavaScript source code
+
+let a = prompt('Please log in')
+
+if (a === 'admin') {
+    let b = prompt('Enter password')
+        if (b === 'password') {
+            alert('You are logged in');
+        } else {
+            alert('wrong password')
+        }
+} else {
+    alert('User unknown')
+}
