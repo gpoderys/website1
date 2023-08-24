73977d6e68bf90912720e5d6718b4395d9eda571
Gintautas Poderys <gintautasp25@gmail.com>
2023-08-24 15:31:39 +0300

    testing Js

--- Script1.js
+++ Script1.js
@@ -7,11 +7,8 @@ newDiv.style.height = '200px'
 bdy.appendChild(newDiv)
 
 const container = document.querySelector('.megaDiv');
-
 const content = document.createElement('p');
-
 content.textContent = "Hey I'm Here!";
-
 container.appendChild(content);
 
 const container2 = document.querySelector('.megaDiv');
@@ -19,3 +16,8 @@ const content2 = document.createElement('h3');
 content2.textContent = "Hey I'm Here too!";
 content2.style.color = 'blue'
 container2.appendChild(content2);
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', function (e) {
+    e.target.style.background = 'blue';
+  });
